f349b9b54270ddbe3d48805c35cd5f8ec4ca651d
Deepesh Gupta <34032722+PrinceDeepu@users.noreply.github.com>
2022-08-21 01:01:46 +0530

    noddy@0.9.2

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -3,20 +3,13 @@ import { Router } from "@deepeshgupta/noddy";
 import { Index } from "../controllers/Index";
 import index from "./index.routes";
 
-const router = new Router();
+const run = (): Router => {
+    const router = new Router();
 
-/**
- * Setting up CORS policy for app
- */
-// router.use({
-//     path: "",
-//     middlewares: cors({
-//         // origin: conf.Config.get("appUrl"),
-//         optionsSuccessStatus: prototypes.httpStatusCodes.OK,
-//     }),
-// });
+    router.use({ path: "/", controller: Index });
+    router.use({ path: "/api", router: index });
 
-router.use({ path: "/", controller: Index });
-router.use({ path: "/api", router: index });
+    return router;
+};
 
-export default router;
+export default run;
